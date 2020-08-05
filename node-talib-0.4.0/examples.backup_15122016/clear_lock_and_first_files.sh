3874f423ee9830bba597b1173ca39fcd261c6689
Doron Shushan <DoronS3@verifone.com>
2020-08-05 10:41:20 +0000

    Revert "first commit"
    This reverts commit f7b32b61e9637d4d6ca95385a5a059393259f105.

--- node-talib-0.4.0/examples.backup_15122016/clear_lock_and_first_files.sh
+++
@@ -1,2 +0,0 @@
-rm -f /IB_TI/node-talib-0.4.0/examples/*.lock; rm -f /IB_TI/node-talib-0.4.0/examples/*.first
- rm -f *.first; rm -f *.lock
--- node-talib-0.4.0/examples/clear_lock_and_first_files.sh
+++
@@ -1,8 +0,0 @@
-#for i in gbp eur cad jpy aud chf nzd; do
-#  if pgrep -f $i > /dev/null; then
-#       echo $i
-#  fi
-#done
-#if [ pgrep -f 
-rm -f /IB_TI/node-talib-0.4.0/examples/*.lock; rm -f /IB_TI/node-talib-0.4.0/examples/*.first
- rm -f *.first; rm -f *.lock
